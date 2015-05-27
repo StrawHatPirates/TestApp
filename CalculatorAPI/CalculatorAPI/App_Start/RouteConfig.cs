c563a4bf0e68ff8fb54a8c41fd2d2ed16360e84c
Parvez Ahmed <parvezthepop@gmail.com>
2015-05-27 21:32:15 +0530

    1.added multiply controller 2. mapped html to json output format

--- CalculatorAPI/CalculatorAPI/App_Start/RouteConfig.cs
+++ CalculatorAPI/CalculatorAPI/App_Start/RouteConfig.cs
@@ -18,6 +18,7 @@ namespace CalculatorAPI
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            
         }
     }
 }
---
+++ CalculatorAPI/CalculatorAPI/Controllers/MultiplyController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CalculatorAPI.Controllers
+{
+    public class MultiplyController : ApiController
+    {
+        [HttpGet]
+        [Route("api/multiply")]
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+    }
+}
